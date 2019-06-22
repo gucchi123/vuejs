fb47f69c5b7567efc1cbddd0247022ee95f43ee8
Mizuguchi, Makoto <makoto.mizuguchi@accenture.com>
2019-06-22 15:15:26 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: "Hello Vue.js",
+    url: "http://54.92.67.141/views/Regional/Economy?iframeSizedToWindow=true&:embed=y&:showAppBanner=false&:display_count=no&:showVizHome=no&:origin=viz_share_link"
+  }
+})
